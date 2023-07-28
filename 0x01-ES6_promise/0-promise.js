ecc1d55e17018d277b5c670f9c3ba4a8900c10e1
Bboy010 <hkoffianderson@gmail.com>
2023-07-27 19:31:24 -0700

    simple promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,19 +1,10 @@
-// Definition of the function prototype getResponseFromAPI()
-function getResponseFromAPI() {
+export default function getResponseFromAPI() {
+  const success = false;
   return new Promise((resolve, reject) => {
-    // Simulate an API call or any asynchronous operation
-    setTimeout(() => {
-      // Simulated API response
-      const apiResponse = { data: "API response data" };
-
-      // Resolve the Promise with the API response
-      resolve(apiResponse);
-
-      // In case of an error during the API call, you can reject the Promise like this:
-      // reject(new Error("API call error"));
-    }, 2000); // Simulate a 2-second delay before receiving the API response
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
   });
 }
-
-export default getResponseFromAPI;
-
