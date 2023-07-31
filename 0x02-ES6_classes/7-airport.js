2bf4c4864a0f4afc4ce36c37d543ba3f42d07a13
Bboy010 <hkoffianderson@gmail.com>
2023-07-30 19:47:56 -0700

    Primitive Holberton Class

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -8,28 +8,11 @@ export default class Airport {
     return this._name;
   }
 
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
-      throw new TypeError('Name must be a string.');
-    }
-  }
-
   get code() {
     return this._code;
   }
 
-  set code(newCode) {
-    if (typeof newCode === 'string') {
-      this._code = newCode;
-    } else {
-      throw new TypeError('Code must be a string.');
-    }
-  }
-
   toString() {
-    return this._code;
+    return `[object ${this._code}]`;
   }
 }
-
