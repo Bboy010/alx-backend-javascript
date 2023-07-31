c81413bce8ee76462aad5d58fe952541a2c4448e
Bboy010 <hkoffianderson@gmail.com>
2023-07-30 20:01:27 -0700

    Primitive Holberton CLass

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -8,32 +8,17 @@ export default class HolbertonClass {
     return this._size;
   }
 
-  set size(newSize) {
-    if (typeof newSize === 'number') {
-      this._size = newSize;
-    } else {
-      throw new TypeError('Size must be a number.');
-    }
-  }
-
   get location() {
     return this._location;
   }
 
-  set location(newLocation) {
-    if (typeof newLocation === 'string') {
-      this._location = newLocation;
-    } else {
-      throw new TypeError('Location must be a string.');
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
     }
-  }
-
-  valueOf() {
-    return this._size;
-  }
-
-  toString() {
-    return this._location;
+    if (hint === 'string') {
+      return this._location;
+    }
+    return this;
   }
 }
-
