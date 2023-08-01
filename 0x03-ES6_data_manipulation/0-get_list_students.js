228303f269f46ce4fcf3862b7a9ed4e37e8196d2
Bboy010 <hkoffianderson@gmail.com>
2023-08-01 16:44:54 -0700

    Basic list of objects)

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,24 +1,7 @@
-// Define the function
 export default function getListStudents() {
   return [
-  // Add student objects to the array
-  {
-    id: 1,
-    firstName: "Guillaume",
-    location: "San Francisco"
-  },
-
-          {
-     id: 2,
-    firstName: "James",
-    location: "Columbia"
-  },
-
- {
-    id: 5,
-    firstName: "Serena",
-    location: "San Francisco"
-  },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-
 }
