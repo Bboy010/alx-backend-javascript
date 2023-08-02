0b3f3a09adc40f74520f8b4e93340ca2b26a0656
Bboy010 <hkoffianderson@gmail.com>
2023-08-01 17:46:46 -0700

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,11 @@
-// Import function getListStudents get the list of student
-import getListStudents from "./0-get_list_students.js";
-
-// Define function updateStudentGradeByCity
-export default function updateStudentGradeByCity(studentsList, city, newGrades) {
-  // Use filter to filtered students by city
-  const studentsInCity = studentsList.filter((student) => student.location === city);
-
-  // Use map to update students by city 
-  const updatedStudents = studentsInCity.map((student) => {
-  const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
-  const updatedGrade = gradeObject ? gradeObject.grade : "N/A";
-  return { ...student, grade: updatedGrade };
-  });
-
-  // Return array updated
-  return updatedStudents;
+export default function getStudentsByLocation(array, city, grad) {
+  return array
+    .filter((i) => i.location === city)
+    .map((student) => {
+      const gradeI = grad
+        .filter((i) => i.studentId === student.id)
+        .map((x) => x.grade)[0];
+      const grade = gradeI || 'N/A';
+      return { ...student, grade };
+    });
 }
