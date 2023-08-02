5fae97980b14e14c60a452a3b181213e826f8fbe
Bboy010 <hkoffianderson@gmail.com>
2023-08-01 17:37:25 -0700

    update

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
-// Define function getStudentsByLocation
 export default function getStudentsByLocation(array, city) {
-  // Use filter function to filtered student by city
 	return array.filter((student) => student.location === city);
 }
