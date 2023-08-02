e27edbfcf398ca3c7f9874c610dc04817fb81d0e
Bboy010 <hkoffianderson@gmail.com>
2023-08-01 17:54:31 -0700

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,9 @@
 export default function getStudentsByLocation(array, city, grad) {
   return array
-    .filter((i) => i.location === city)
+    .filter((stud) => stud.location === city)
     .map((student) => {
       const gradeI = grad
-        .filter((i) => i.studentId === student.id)
+        .filter((stud) => stud.studentId === student.id)
         .map((x) => x.grade)[0];
       const grade = gradeI || 'N/A';
       return { ...student, grade };
