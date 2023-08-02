aee4a1283acebeae3192b6457a4ee55e72476eb0
Bboy010 <hkoffianderson@gmail.com>
2023-08-01 17:02:35 -0700

    Filter function

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,11 +1,5 @@
-// Import function getListStudents to get the list of students
-import getListStudents from "./0-get_list_students.js";
-
 // Define function getStudentsByLocation
 export default function getStudentsByLocation(studentsList, city) {
   // Use filter function to filtered student by city
-  const studentsInCity = studentsList.filter((student) => student.location === city);
-
-  // Return array of student by city specified
-  return studentsInCity;
+return studentsList.filter((student) => student.location === city);
 }
