85f306cac49350d4669bf0c8779e7886b70b755e
Bboy010 <hkoffianderson@gmail.com>
2023-08-01 17:19:09 -0700

    Reduce to sum all ids

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,11 +1,5 @@
-// Import function getListStudents for getting the lists of students
-import getListStudents from "./0-get_list_students.js";
-
 // Definition of function getStudentIdsSum
 export default function getStudentIdsSum(studentsList) {
   // Use function reduce to get the sum of students ids
-  const idsSum = studentsList.reduce((sum, student) => sum + student.id, 0);
-
-  // Return the sum of students ids
-  return idsSum;
+return studentsList.reduce((sum, student) => sum + student.id, 0);
 }
