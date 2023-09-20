1636270ee7746bbd5672e16f30b7edaf0b3861dc
Bboy010 <hkoffianderson@gmail.com>
2023-09-20 16:55:30 -0700

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,41 @@
+// Import the 'readline' module to read user input from the command line
 const readline = require('readline');
 
+// Create an interface for reading and writing to the command line
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+  input: process.stdin, // Read input from standard input (keyboard)
+  output: process.stdout, // Write output to standard output (console)
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  rl.close();
-});
+// Check if the program is running in interactive mode (user provides input)
+if (process.stdin.isTTY) {
+  // Step 1: Display a welcome message
+  console.log('Welcome to Holberton School, what is your name?');
+
+  // Step 2: Prompt the user for their name
+  rl.question('', (name) => {
+    // Step 3: Display the user's name
+    console.log(`Your name is: ${name}`);
+
+    // Close the interface and exit the program
+    rl.close();
+  });
+} else {
+  // Step 2: Read the user's name from standard input (piped input)
+  let name = '';
+
+  process.stdin.on('data', (chunk) => {
+    name += chunk.toString();
+  });
+
+  process.stdin.on('end', () => {
+    // Remove any leading/trailing whitespace and line breaks
+    name = name.trim();
+
+    // Step 3: Display the user's name
+    console.log(`Your name is: ${name}`);
+
+    // Step 4: Display a closing message
+    console.log('This important software is now closing');
+  });
+}
